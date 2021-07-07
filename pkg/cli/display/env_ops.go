e77e65d4a856b94699e945052d43332b3399da3d
Liu Cong <innerr@gmail.com>
2021-07-07 18:26:42 +0800

    better args hint with abbrs in mapping
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- pkg/cli/display/env_ops.go
+++ pkg/cli/display/env_ops.go
@@ -28,6 +28,7 @@ func DumpEnvOpsCheckResult(
 		}
 	}
 
+	var cic *core.Cmd
 	var arg2env *core.Arg2Env
 	isArg2EnvCanFixAllFatals := (len(cmds) == 1 && len(fatals.result) != 0)
 	if isArg2EnvCanFixAllFatals {
@@ -35,7 +36,7 @@ func DumpEnvOpsCheckResult(
 		if cmd == nil || cmd.Cmd() == nil {
 			isArg2EnvCanFixAllFatals = false
 		} else {
-			cic := cmd.Cmd()
+			cic = cmd.Cmd()
 			arg2env = cic.GetArg2Env()
 			for _, it := range fatals.result {
 				if !arg2env.Has(it.Key) {
@@ -115,6 +116,8 @@ func DumpEnvOpsCheckResult(
 		}
 	}
 
+	abbrsSep := env.GetRaw("strs.abbrs-sep")
+
 	if len(fatals.result) != 0 {
 		for i, it := range fatals.result {
 			if i != 0 {
@@ -128,7 +131,18 @@ func DumpEnvOpsCheckResult(
 			prti("- but not provided.", 7)
 			if arg2env != nil && arg2env.Has(it.Key) {
 				prti("- the arg below is mapped to this key, pass it to solve this error:", 7)
-				prti("'"+arg2env.GetArgName(it.Key)+"'", 12)
+				argName := arg2env.GetArgName(it.Key)
+				argInfo := "'" + argName + "'"
+				args := cic.Args()
+				abbrs := args.Abbrs(argName)
+				if len(abbrs) > 1 {
+					abbrTerm := "abbr"
+					if len(abbrs) > 2 {
+						abbrTerm = "abbrs"
+					}
+					argInfo += " (" + abbrTerm + ": " + strings.Join(abbrs[1:], abbrsSep) + ")"
+				}
+				prti(argInfo, 12)
 			}
 		}
 
