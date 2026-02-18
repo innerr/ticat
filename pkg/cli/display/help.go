d7fb4aeb4a56eb62fb604e86c6ba57e53b5622b2
Liu Cong <innerr@gmail.com>
2026-02-19 04:46:28 +0800

    test: add UTs for env changing in flow stack
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- pkg/cli/display/help.go
+++ pkg/cli/display/help.go
@@ -1,71 +1,12 @@
 package display
 
 import (
-	"strings"
-
 	"github.com/innerr/ticat/pkg/core/model"
-	"github.com/innerr/ticat/pkg/utils"
 )
 
 func PrintGlobalHelp(cc *model.Cli, env *model.Env) {
 	// TODO: use man page instead of help
 	PrintSelfHelp(cc.Screen, env)
-	return
-
-	if len(cc.Helps.Sections) == 0 {
-		PrintSelfHelp(cc.Screen, env)
-		return
-	}
-
-	// TODO: with color output this is not right, disable it by setting to a very big value
-	_, width := utils.GetTerminalWidth(50, 100)
-	width = 4096
-
-	pln := func(line string) {
-		line = DecodeColor(line, env)
-		if len(line) <= width {
-			_ = cc.Screen.Print(line + "\n")
-			return
-		}
-
-		indent := 0
-		for _, char := range line {
-			if char != ' ' && char != '\t' {
-				break
-			}
-			indent += 1
-		}
-		prefix := line[:indent]
-
-		printWithPrefix := func(printed bool, line string) {
-			if printed {
-				_ = cc.Screen.Print(prefix)
-				_ = cc.Screen.Print(strings.TrimLeft(line, " \t") + "\n")
-			} else {
-				_ = cc.Screen.Print(line + "\n")
-			}
-		}
-
-		printed := false
-		for {
-			if len(line) > width {
-				printWithPrefix(printed, line[:width])
-				line = line[width:]
-				printed = true
-				continue
-			} else {
-				printWithPrefix(printed, line)
-			}
-			break
-		}
-	}
-
-	for _, help := range cc.Helps.Sections {
-		PrintTipTitle(cc.Screen, env, help.Title)
-		for _, line := range help.Text {
-			pln(line)
-		}
-	}
 }
 
 func PrintSelfHelp(screen model.Screen, env *model.Env) {
