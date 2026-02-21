81bdefbb70cd455bfb246647e70fa4c0a43f9221
Liu Cong <innerr@gmail.com>
2026-02-21 18:07:03 +0800

    feat(parser): add help flag transform (-h, --help, -?, ?, -help)
    Transform help flags at the end of command line to appropriate commands:
    - ticat -h/--help/-?/?/-help -> ticat help
    - ticat {cmd} -h/... -> ticat cmd {cmd}
    - ticat {cmd1} : {cmd2} : ... -h/... -> ticat desc.more : {cmd1} : {cmd2} : ...
    
    This works with both space-separated (dm : dm) and no-space (dm:dm) formats.
    
    Added 112 test cases covering all scenarios.

--- pkg/core/parser/cmds.go
+++ pkg/core/parser/cmds.go
@@ -1,26 +1,108 @@
 package parser
 
 import (
+	"strings"
+
 	"github.com/innerr/ticat/pkg/core/model"
 )
 
 type Parser struct {
 	seqParser *SequenceParser
 	cmdParser *CmdParser
+	helpFlags []string
+	sep       string
+	envPrefix string
+	envSuffix string
 }
 
-// UPDATE: rewite this with goyacc
-//
-// A very simple implement of command line parsing, lack of char escaping
-//   - The command line argv list have extra tokenizing info
-//   - An example: a quoted string with space inside
-//   - TODO: how to store this info(to flow file?) and still keep it human-editable ?
-//   - The dynamite info(registered modules and env KVs) could use for disambiguation
-//   - Inconvenient to use a LEX/YACC lib to parse
+func NewParser(seqParser *SequenceParser, cmdParser *CmdParser) *Parser {
+	return &Parser{
+		seqParser: seqParser,
+		cmdParser: cmdParser,
+		helpFlags: []string{"-h", "--help", "-?", "?", "-help"},
+		sep:       ":",
+		envPrefix: "{",
+		envSuffix: "}",
+	}
+}
+
+func (self *Parser) isEnvBlock(arg string) bool {
+	return strings.HasPrefix(arg, self.envPrefix) && strings.HasSuffix(arg, self.envSuffix)
+}
+
+func (self *Parser) transformHelpFlag(input []string) []string {
+	if len(input) == 0 {
+		return input
+	}
+
+	lastArg := input[len(input)-1]
+	isHelpFlag := false
+	for _, hf := range self.helpFlags {
+		if lastArg == hf {
+			isHelpFlag = true
+			break
+		}
+	}
+	if !isHelpFlag {
+		return input
+	}
+
+	args := self.seqParser.Normalize(input[:len(input)-1])
+
+	if len(args) == 0 {
+		return []string{"help"}
+	}
+
+	globalEnvs := []string{}
+	remainingArgs := args
+	for len(remainingArgs) > 0 && self.isEnvBlock(remainingArgs[0]) {
+		globalEnvs = append(globalEnvs, remainingArgs[0])
+		remainingArgs = remainingArgs[1:]
+	}
+
+	if len(remainingArgs) == 0 {
+		return append(globalEnvs, "help")
+	}
+
+	firstNonSepIdx := -1
+	for i, arg := range remainingArgs {
+		if arg != self.sep {
+			firstNonSepIdx = i
+			break
+		}
+	}
+
+	if firstNonSepIdx == -1 {
+		return append(globalEnvs, "help")
+	}
+
+	hasCmdSequence := false
+	for i := firstNonSepIdx; i < len(remainingArgs); i++ {
+		if remainingArgs[i] == self.sep {
+			hasCmdSequence = true
+			break
+		}
+	}
+
+	if hasCmdSequence {
+		result := append(globalEnvs, "desc", ".", "more")
+		for _, arg := range remainingArgs {
+			if arg != self.sep {
+				result = append(result, self.sep, arg)
+			}
+		}
+		return result
+	}
+
+	return append(globalEnvs, append([]string{"cmd"}, remainingArgs...)...)
+}
+
 func (self *Parser) Parse(
 	cmds *model.CmdTree,
 	envAbbrs *model.EnvAbbrs,
 	input ...string) *model.ParsedCmds {
+
+	input = self.transformHelpFlag(input)
 
 	seqs, firstIsGlobal := self.seqParser.Parse(input)
 	flow := model.ParsedCmds{GlobalEnv: model.ParsedEnv{}, Cmds: nil, GlobalCmdIdx: -1}
@@ -38,8 +120,4 @@ func (self *Parser) Parse(
 		}
 	}
 	return &flow
-}
-
-func NewParser(seqParser *SequenceParser, cmdParser *CmdParser) *Parser {
-	return &Parser{seqParser, cmdParser}
 }
